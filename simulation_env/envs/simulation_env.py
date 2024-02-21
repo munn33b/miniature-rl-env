b5602e3e898e0852c2823783b02d3c3527b2def1
munn33b <muneeburrehman197@gmail.com>
2024-02-21 05:36:48 -0500

    Code Update

--- simulation_env/envs/simulation_env.py
+++ simulation_env/envs/simulation_env.py
@@ -15,19 +15,22 @@ class SimulationEnv(gym.Env):
         self.image = self.bridge.imgmsg_to_cv2(camera_img_msg, "bgr8")
     def _get_observation(self):
         rospy.Subscriber("/miniature_robot/camera/image_raw", Image, self.camera_cb)
+        time.sleep(0.1)
+        self.image_array = np.asarray(self.image)
+        return self.image_array
     def __init__(self):
         self.bridge = CvBridge()
         rospy.init_node("simulation_env")
-        self._get_observation()
-        time.sleep(0.1)
-        self.image_array = np.asarray(self.image)
+        self.image_initial = rospy.wait_for_message("/miniature_robot/camera/image_raw", Image, timeout=2)
+        self.image_initial_cv2 = self.bridge.imgmsg_to_cv2(self.image_initial, "bgr8")
+        self.image_array = np.asarray(self.image_initial_cv2)
         print (self.image_array.shape)
         self.observation_space = spaces.Box(low=0, high=255, shape=self.image_array.shape, dtype=np.uint8)
     def reset(self):
-        return self.observation_space
+        self.current_observation = self._get_observation()
+        return self.current_observation
     def render(self, mode=None):
-        cv2.imshow("Output", self.image)
-        cv2.waitKey(1)
+        self._get_observation()
     def step(self):
         current_observation = self._get_observation(self)
         return current_observation
